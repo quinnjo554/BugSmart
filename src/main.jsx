929a9a9608b1eaea3cad2b9b809122a7ef69b48f
Quinn Johnson <quinnjohnson24@gmail.com>
2023-02-03 01:07:09 -0600

    Added authentication and hero info card

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import { Auth0Provider } from "@auth0/auth0-react";
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <Auth0Provider
+    domain="Domain"
+    clientId="ADDCLinetid"
+    authorizationParams={{
+      redirect_uri: "http://localhost:5173/MainPage"
+    }}
+    >
     <App />
+    </Auth0Provider>
   </React.StrictMode>,
 )
